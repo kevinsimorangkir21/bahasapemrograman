d5c04e94a9a51cf2eeae2a0ca93c7e25dbaa0bcf
VIN <104813623+kevinsimorangkir21@users.noreply.github.com>
2022-07-03 15:28:49 +0700

    ini algoritma matematika di cpp

---
+++ algoritma_mtk.cpp
@@ -0,0 +1,24 @@
+//Misalkan diminta penjumlahan 5 + 6 = 11 dibuat dalam cpp
+
+#include<iostream>
+using namespace std;
+
+int main()
+  int x; //variabel pertama bilangan
+  int y; //variabel kedua bilangan
+  
+  cin>>x;  //menginputkan bilangan pertama
+  cin>>y;  //menginputkan bilangan kedua
+  
+//algoritma matematika
+//+ -> penjumlahan
+//- -> pengurangan
+// / -> pembagian
+//* -> perkalian
+
+  hasil = x + y;  //karena penjumlahan
+  hasil = x * y;  //karena perkalian
+  hasil = x / y   //karena pembagian
+  hasil = x - y   //karena pengurangan
+    
+return 0;
