629cad4e480a95aa956271de163b8c4e9db5a45c
VIN <104813623+kevinsimorangkir21@users.noreply.github.com>
2022-07-03 15:21:21 +0700

    nambah cin

---
+++ cin.cpp
@@ -0,0 +1,15 @@
+//cin adalah sebuah instruksi untuk menginputkan data pada sebuah instruksi cout
+//misalkan pada pengisian identitas diri pada formulir
+
+#include<iostream>
+using name space std;
+
+int main()
+  int x;
+  
+  cout<<"Nama Mahasiswa";  //instruksi awalan data
+  cin>>x;                  //instruksi menginputkan data
+
+//x disebut variabel -> x bisa diganti dengan huruf yang lain bukan dengan huruf tapi dengan kalimat singkat contoh, nama
+
+return 0;
